4cfe79f946094c6a8724978773c5e75f8c6d6282
App Academy Student <student@appacademy.io>
2017-08-22 17:57:41 -0700

    started custom validation

--- Cats/app/models/cat_rental_request.rb
+++ Cats/app/models/cat_rental_request.rb
@@ -1,6 +1,20 @@
 class CatRentalRequest < ApplicationRecord
   validates :status, :includes ["PENDING", "APPROVED", "DENIED"]
   validates :cat_id, :start_date, :end_date, :status, presence: true
+  # validates :
 
   belongs_to :cat, dependent: :destroy
+
+  def overlapping_requests
+    range = DBConnection.execute(<<-SQL)
+      SELECT
+
+      FROM
+
+      JOIN cats
+        ON cat.id = cat_rental_requests.cat_id
+
+      WHERE
+    SQL
+  end
 end
